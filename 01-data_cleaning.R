2d707f3f60ff5525e7954d952bc5f24388c52ca7
jilbjpb <99112011+jilbjpb@users.noreply.github.com>
2022-02-06 23:15:00 +0800

    Update 01-data_cleaning.R

--- 01-data_cleaning.R
+++ 01-data_cleaning.R
@@ -15,21 +15,9 @@
 # Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
-
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+data_clean <-data%>%filter(!is.na(SCORE)&!is.na(ENTRANCE_LOBBY))
          
 
-#### What's next? ####
 
 
 
